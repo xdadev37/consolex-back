5f384b1c4f4ef168dc385a1b41a1d3fb19d120a8
xdadev37 <pouriasa37@gmail.com>
2022-09-19 13:52:40 +0430

    created new strapi proj;

--- src/consolex-back/config/server.ts
+++ src/consolex-back/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: ["4696048pouria"],
+    keys: env.array('APP_KEYS'),
   },
 });
